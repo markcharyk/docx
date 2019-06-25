4411b9c23659dd35415bcba1323798cf518407be
Mark Charyk <markcharyk@gmail.com>
2019-06-25 08:08:08 -0700

    Add the build folder to the git repo
    We add the build folder so that we're able to point to a JS version of this project via commit hash

---
+++ build/file/table/table-row/table-row-properties.d.ts
@@ -0,0 +1,12 @@
+import { IgnoreIfEmptyXmlComponent, XmlComponent } from "../../../file/xml-components";
+export declare class TableRowProperties extends IgnoreIfEmptyXmlComponent {
+    constructor();
+    setCantSplit(): TableRowProperties;
+    setTableHeader(): TableRowProperties;
+}
+export declare class CantSplit extends XmlComponent {
+    constructor();
+}
+export declare class TableHeader extends XmlComponent {
+    constructor();
+}
---
+++ build/file/table/table-row/table-row.d.ts
@@ -0,0 +1,12 @@
+import { XmlComponent } from "../../../file/xml-components";
+import { TableCell } from "../table-cell";
+export declare class TableRow extends XmlComponent {
+    private readonly cells;
+    private readonly properties;
+    constructor(cells: TableCell[]);
+    getCell(index: number): TableCell;
+    addGridSpan(index: number, cellSpan: number): TableCell;
+    mergeCells(startIndex: number, endIndex: number): TableCell;
+    setCantSplit(): TableRow;
+    setTableHeader(): TableRow;
+}
